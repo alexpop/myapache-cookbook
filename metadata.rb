59eec2d692627cc3ddeb58ad0ad42d1cf73daf14
Alex Pop <al3xpop@gmail.com>
2014-10-08 15:00:05 -0700

    commit .10

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'alex@example.com'
 license          'All rights reserved'
 description      'Installs/Configures myapache-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.1.10'
 
 recipe 'myapache-cookbook::default', 'Default recipe to install Apache'
