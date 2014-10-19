35e1e627883fb2bc85a602f947e1575c7259894d
Alex Pop <al3xpop@gmail.com>
2014-10-19 23:32:57 +0100

    added kitchen yml

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,6 @@ when 'rhel'
   default['myapache-cookbook']['package'] = 'httpd'
 end
 
-default['myapache-cookbook']['apache']['port'] = 80
-default['myapache-cookbook']['name'] = 'John'
 default['myapache-cookbook']['doc-root'] = '/var/www/html'
 default['myapache-cookbook']['git-repo'] = 'https://github.com/alexpop/myapache-app'
 default['myapache-cookbook']['git-revision'] = 'master'
