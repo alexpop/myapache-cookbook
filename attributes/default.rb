f4b4f10252a2a728ef13e6498252c43020aca1b0
Alex Pop <al3xpop@gmail.com>
2015-01-14 17:56:52 +0000

    added release versioning

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['myapache-cookbook']['doc-root'] = '/var/www/html'
-default['myapache-cookbook']['git-repo'] = 'https://github.com/alexpop/myapache-app'
+default['myapache-cookbook']['git-repo'] = 'https://github.com/alexpop/myhtml-app'
 default['myapache-cookbook']['git-revision'] = 'master'
