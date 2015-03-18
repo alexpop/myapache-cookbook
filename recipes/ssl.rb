8acd4d7704c27e1c3168b7c37cc817fc75be3c4a
Alex Pop <al3xpop@gmail.com>
2015-03-18 19:21:03 +0200

    ssl broken

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -24,6 +24,7 @@ file '/etc/ssl/certificate.crt' do
   mode '0600'
   sensitive true
   action :create
+  notifies :run, 'ruby_block[ssl_script]'
 end
 
 file '/etc/ssl/certificate.key' do
@@ -31,6 +32,21 @@ file '/etc/ssl/certificate.key' do
   mode '0600'
   sensitive true
   action :create
+end
+
+# Only executed when the cert file is created/updated
+ruby_block "ssl_script" do
+  block do
+    cert = OpenSSL::X509::Certificate.new(File.read('/etc/ssl/certificate.crt'))
+    expire_days = ((cert.not_after-Time.now)/86400).to_i
+    node.set['ssl_cert']['not_after'] = cert.not_after
+    node.set['ssl_cert']['expire_days'] = expire_days
+    if expire_days<=30
+      raise "Only SSL certificates above 30 days from expiration are allowed"+
+            "Number of days left: #{expire_days}. Negative number indicates expired certificate"
+    end
+  end
+  action :nothing
 end
 
 # Old approach before chef-client 12
