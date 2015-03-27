ac6efa17c8a137fc378a0326b51e739b8538489e
Alex Pop <al3xpop@gmail.com>
2015-03-27 09:04:25 +0000

    improved audit recipe

--- recipes/audit_recipe.rb
+++ recipes/audit_recipe.rb
@@ -6,19 +6,36 @@
 log "*** In audit_recipe!"
 
 control_group "Apache Audit" do
+  describe file('/etc/passwd') do
+    it { should contain 'apache' }
+  end
 
   control "httpd package" do
-    it "should not be installed" do
-      expect(package("httpd")).to_not be_installed
+    it "should be installed" do
+      expect(package("httpd")).to be_installed
     end
   end
  
+  describe command('apachectl -S') do
+    its(:stdout) { should match /^Syntax OK/ }
+  end
+
   describe port(80) do
     it { should be_listening }
   end
-
-	describe port(12345) do
-    it { should be_listening }
-	end
-
 end
+
+control_group "System Audit" do
+  iptables_command='chkconfig --list iptables'
+  describe command(iptables_command) do
+    its(:stdout) { should_not match /^iptables.+:on/ }
+  end
+
+  describe port(12345) do
+    it { should be_listening }
+  end
+end
+
+
+
+
