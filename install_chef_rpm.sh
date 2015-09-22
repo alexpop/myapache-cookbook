c376c779f530b34f3c837ba9a3c991c217c88b02
Alex Pop <al3xpop@gmail.com>
2015-09-22 15:59:32 +0300

    instal updated

--- install_chef_rpm.sh
+++ install_chef_rpm.sh
@@ -3,4 +3,4 @@
 
 # Pick the right EL binary
 major=`cat /etc/redhat-release | cut -d" " -f3 | cut -d "." -f1`
-rpm -Uvh --oldpackage --replacepkgs "/mnt/share/chef/chef-12.3.0-1.el$major.x86_64.rpm"
+rpm -Uvh --oldpackage --replacepkgs "/mnt/share/chef/chef-12.4.1-1.el$major.x86_64.rpm"
